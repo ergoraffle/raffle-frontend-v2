b9210f0277c1666856c5e079ef7de09d030dc0d0
Ali Ghafoorzade <alighafoorzade@gmail.com>
2025-08-26 11:06:10 +0000

    Feat/setup prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "Ergo Raffle",
-  description: "Ergo raffle is a crowdfunding system based on ergo contracts",
+  title: 'Ergo Raffle',
+  description: 'Ergo raffle is a crowdfunding system based on ergo contracts',
 };
 
 export default function RootLayout({
@@ -13,11 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`antialiased`}>{children}</body>
     </html>
   );
 }
