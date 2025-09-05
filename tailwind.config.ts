e3351b05b99ff453e3a21ddebc615e8ee7b58e91
alighafoorzade <alighafoorzade@gmail.com>
2025-09-05 14:53:36 +0330

    feat: add base colors, spacing, text size and radius

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+const config = {
+  theme: {
+    colors: {
+      black: 'var(--color-black)',
+      white: 'var(--color-white)',
+      orange: 'var(--color-orange)',
+      red: 'var(--color-red)',
+      yellow: 'var(--color-yellow)',
+      green: 'var(--color-green)',
+    },
+  },
+  plugins: [],
+};
+export default config;
