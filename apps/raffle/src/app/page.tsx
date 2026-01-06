1d19192d5f770ab7cf33a080befb4fa1a06605ab
m.bahrami <m.bahrami>
2026-01-06 17:04:29 +0330

    add svg icon files

--- apps/raffle/src/app/page.tsx
+++ apps/raffle/src/app/page.tsx
@@ -1,5 +1,11 @@
+import { Ergo } from '@ergo-raffle/icons';
 import { Typography } from '@ergo-raffle/ui-kit';
 
-const Home = () => <Typography>Home Page</Typography>;
+const Home = () => (
+  <Typography>
+    <Ergo className="h-6 w-6" />
+    Home Page
+  </Typography>
+);
 
 export default Home;
