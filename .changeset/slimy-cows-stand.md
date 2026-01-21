a2063b3bde10fd70ab11b2ef76da8b3dce2903cd
Mahtab <m.bahrami448@gmail.com>
2026-01-21 15:08:32 +0330

    feat: add typography variants

---
+++ .changeset/slimy-cows-stand.md
@@ -0,0 +1,5 @@
+---
+"@ergo-raffle/ui-kit": minor
+---
+
+Add the typography component and its variants to the ui-kit package. Define variants as reusable CSS classes so other components use them directly.
--- apps/raffle/src/app/layout.tsx
+++ apps/raffle/src/app/layout.tsx
@@ -4,13 +4,22 @@ import './globals.css';
 
 import type { PropsWithChildren } from 'react';
 
+import { fraunces, karla, poppins } from './fonts';
+
 export const metadata: Metadata = {
   title: 'Ergo Raffle',
   description: 'Ergo raffle is a crowdfunding system based on ergo contracts'
 };
 
 const RootLayout = ({ children }: PropsWithChildren) => (
-  <html lang="en">
+  <html
+    lang="en"
+    className={`
+        ${fraunces.variable}
+        ${poppins.variable}
+        ${karla.variable}
+      `}
+  >
     <body className="antialiased">{children}</body>
   </html>
 );
