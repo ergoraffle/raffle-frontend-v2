d3887116932f060dabb847477c884cad16f1197b
Mahtab Bahrami <m.bahrami448@gmail.com>
2026-02-16 13:07:33 +0000

    feat: add token display component with icon

--- packages/icons/vite.config.ts
+++ packages/icons/vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [dts({ entryRoot: './lib' }), svgr()],
+  plugins: [
+    dts({ entryRoot: './lib' }),
+    svgr({ svgrOptions: { expandProps: 'end' } }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'lib/tokens/*.*',
+          dest: 'tokens'
+        }
+      ]
+    })
+  ],
   build: {
     lib: {
       entry: './lib/index.ts',
