79dd92cb11b3debeee9794777520ed683debc1a1
Masood Abdolian <m.abdolian@gmail.com>
2026-04-19 11:01:58 +0330

    update

--- apps/raffle/src/features/CreateRaffle/CreateRaffle.tsx
+++ apps/raffle/src/features/CreateRaffle/CreateRaffle.tsx
@@ -9,7 +9,6 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { FormProvider, useForm } from 'react-hook-form';
 
 import { useInfoBlockchain } from '@/hooks/useInfoBlockchain';
-import { createRaffle } from '@/mockApi';
 
 import { createRaffleSchema, type RaffleForm } from '../schemas';
 import { BasketsForm } from './BasketsForm';
@@ -17,11 +16,19 @@ import { DonationGoalForm } from './DonationGoalForm';
 import { Finish } from './Finish';
 import { SpecificationsForm } from './SpecificationsForm';
 
+import { CreationProxyTxBuilder } from '@ergo-raffle/proxy-transactions';
+
+import { useWallet } from '@/hooks';
+import { NautilusWalletAddresses, UnsignedErgoTxProxy } from '@ergo-raffle/nautilus-wallet';
+
 export const CreateRaffle = () => {
   const [activeStepIndex, setActiveStepIndex] = useState(0);
   const { data: infoBlockchainData } = useInfoBlockchain();
 
+  const wallet = useWallet();
+
   const raffleSchema = createRaffleSchema(infoBlockchainData?.fee?.implementer);
+
   const form = useForm<RaffleForm>({
     resolver: zodResolver(raffleSchema),
     shouldUnregister: false,
@@ -67,8 +74,79 @@ export const CreateRaffle = () => {
       content: <Finish handleBack={handleBack} />
     }
   ];
-  const onSubmit = (data: RaffleForm) => {
-    createRaffle(data);
+
+  const onSubmit = async (data: RaffleForm) => {
+    if (!infoBlockchainData) {
+      throw new Error('TODO');
+    }
+
+    if (wallet?.selected?.name !== 'Nautilus') {
+      throw new Error('TODO');
+    }
+
+    try {
+      const tokens = await wallet.selected.fetchTokens();
+
+      const token = tokens.find((token) => token.id === data.tokenId);
+
+      if (!token) {
+        throw new Error('TODO');
+      }
+
+      const organizerAddress = (wallet.addresses as NautilusWalletAddresses).main;
+
+      const boxes = await wallet.selected.getBoxes();
+
+      const images = data.images?.filter((image) => !!image.url).map((image) => image.url || '') || [];
+
+      const winnerCount = data.details.reduce((result, current) => result + current.count, 0);
+
+      const winnersPercentList: bigint[] = [];
+
+      for (const current of data.details) {
+        for (let i = 0; i < current.count; i++) {
+          winnersPercentList.push(BigInt(current.percent) * 1000n);
+        }
+      }
+
+      const builder = (new CreationProxyTxBuilder())
+        .setChainHeight(infoBlockchainData.height)                                                         // Current chain height
+        .setFeeBoxes(boxes.values())                                                                       // Organizer UTXO boxes the selector may spend
+        .setOrganizerAddress(organizerAddress)                                                             // organizer address is the wallet address that is creating the raffle and will receive the change
+        .setImplementerAddress(process.env.NEXT_PUBLIC_IMPLEMENTER_ADDRESS || '')                          // implementer address is the address of the service that is implementing the raffle
+        .setExpirationHeight(infoBlockchainData.height + Number(process.env.NEXT_PUBLIC_EXPIRATION_HEIGHT))// block height at which the proxy box expires (configure a value for expiration period and add it to the current network height)
+        .setName(data.name)
+        .setDescription(data.description || '')
+        .setTags(data.tags?.join(',') || '')                                                                // Tags string (use empty string when none)
+        .setPictures(images)
+        // .setTicketPrice(ticketPrice)                                                                        // Ticket price in nanoERG or collecting-token units
+        // .setGoal(BigInt(data.count))                                                                        // Funding goal in nanoERG or collecting-token units
+        // .setWinnersPercent(winnersPercent)                                                                  // Total winners share in thousandths
+        // .setWinnerCount(winnerCount)                                                                        // Number of winners
+        // .setWinnersPercentList(winnersPercentList)                                                          // array of winners percentages in thousandths (should sum to 1000)
+        .setRaffleDeadline(data.deadline)                                                                    // Raffle deadline height
+        .setProjectAddress(data.address)                                                                    // Optional project address (defaults to organizer when omitted)
+        // .setCreationFee(BigInt(infoBlockchainData.fee.tx))                                                  // Service creation fee in nanoERG
+        // .setTxFee(BigInt(infoBlockchainData?.fee.tx));                                                      // Transaction fee in nanoERG
+ 
+      // if the raffle is a token-goal raffle, set the collecting token id
+      // if (collectingTokenId !== undefined) {
+      //   builder = builder.setCollectingTokenId(collectingTokenId);
+      // }
+
+      debugger
+
+      const unsignedTx = await builder.build();
+
+      const eip12Object = unsignedTx.toEIP12Object();
+
+      await wallet.selected.transfer(eip12Object as UnsignedErgoTxProxy)
+
+      alert('success')
+    } catch (error) {
+      debugger
+      console.log('error on create raffle', error)
+    }
   };
   return (
     <>
@@ -87,6 +165,30 @@ export const CreateRaffle = () => {
         <CardContent>
           <FormProvider {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)}>
+              <button
+                type='button' 
+                onClick={() => onSubmit({
+                  address: "9hagwneHKMBJHN7pnR8WLwD2ne5NQpWWECLEdwxbS9Bm3iigmkZ",
+                  amount: 5,
+                  count: 54,
+                  deadline: 4,
+                  description: "<p>erer</p>",
+                  details: [{
+                    count: 1,
+                    id: "5d392f24-3645-4067-8a22-a3bd25d02cc2",
+                    percent: 100
+                  }],
+                  tags: [],
+                  eligibility: true,
+                  emptyBaskets: 4,
+                  images: [],
+                  missionFund: 4,
+                  name: "erere",
+                  terms: true,
+                  tokenId: "erg",
+                  winnerPotShare: 4,
+                })}
+              >create</button>
               <div className="mx-auto max-w-7xl">{steps[activeStepIndex]?.content}</div>
             </form>
           </FormProvider>
