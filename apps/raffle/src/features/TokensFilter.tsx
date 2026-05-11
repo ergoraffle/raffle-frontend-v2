da9fd4e84257137058e51b40ca1e0d766141cf58
Mahtab Bahrami <m.bahrami448@gmail.com>
2026-05-11 08:12:30 +0000

    feat: connect raffle list token filter to api

---
+++ apps/raffle/src/features/TokensFilter.tsx
@@ -0,0 +1,45 @@
+import { useMemo, useState } from 'react';
+
+import { MultiSelectCombobox } from '@ergo-raffle/ui-kit';
+
+import { useDebounceString, useFetchTokensSearch } from '@/hooks';
+
+export type TokenFilterProps = {
+  value: string[];
+  onChange: (values: string[]) => void;
+};
+
+export const TokensFilter = ({ value, onChange }: TokenFilterProps) => {
+  const [tokensQuery, setTokensQuery] = useState<string>('');
+  const debouncedTokensQuery = useDebounceString(tokensQuery);
+  const filters = useMemo(
+    () => ({ query: debouncedTokensQuery, limit: 100, offset: 0 }),
+    [debouncedTokensQuery]
+  );
+  const { data: tokensFilterData, isLoading } = useFetchTokensSearch(filters);
+
+  const tokenItems = useMemo(
+    () =>
+      tokensFilterData?.items.map((item) => ({
+        value: item.id,
+        label: item.name ?? item.id
+      })) ?? [],
+    [tokensFilterData]
+  );
+
+  return (
+    <MultiSelectCombobox
+      items={tokenItems}
+      selected={value ?? []}
+      onChange={onChange}
+      placeholder="Token"
+      closeOnChange
+      searchable
+      query={tokensQuery}
+      onQueryChange={setTokensQuery}
+      minQueryLength={3}
+      isLoading={!!tokensQuery.length && isLoading}
+      className="flex-1 lg:flex-auto"
+    />
+  );
+};
