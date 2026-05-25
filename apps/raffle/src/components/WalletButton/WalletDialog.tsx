864f1e15f7c6c9bc6101dcdb12fd9393e234e382
Masood Abdolian <m.abdolian@gmail.com>
2026-05-25 11:27:05 +0330

    update

--- apps/raffle/src/components/WalletButton/WalletDialog.tsx
+++ apps/raffle/src/components/WalletButton/WalletDialog.tsx
@@ -1,38 +1,24 @@
 'use client';
 
-import { type ReactNode, useMemo } from 'react';
-
-import { Left } from '@ergo-raffle/icons';
-import { Button } from '@ergo-raffle/ui-kit';
+import { useMemo } from 'react';
 
 import { useWallet } from '@/hooks';
 
 import { Agreement } from './Agreement';
 import { ChooseWallet } from './ChooseWallet';
 import { ConnectWalletDialog } from './ConnectWalletDialog';
-import { FallbackAddress } from './FallbackAddress';
 
 export const WalletDialog = () => {
   const wallet = useWallet();
 
-  const state = useMemo<'agreement' | 'wallet' | 'ergoAddress'>(() => {
+  const state = useMemo<'agreement' | 'wallet'>(() => {
     if (!wallet.agreed) return 'agreement';
-
-    if (wallet.candidate === 'Nautilus') return 'wallet';
-
-    if (wallet.candidate) return 'ergoAddress';
-
     return 'wallet';
   }, [wallet]);
 
   const steps: Record<
-    'agreement' | 'wallet' | 'ergoAddress',
-    {
-      component: React.ReactNode;
-      title: string | ReactNode;
-      description?: string;
-      dialogMinWidth?: string;
-    }
+    'agreement' | 'wallet',
+    { component: React.ReactNode; title: string; description?: string }
   > = {
     agreement: {
       component: <Agreement />,
@@ -42,23 +28,6 @@ export const WalletDialog = () => {
       component: <ChooseWallet />,
       title: 'Connect Wallet',
       description: 'Choose how you want to connect.'
-    },
-    ergoAddress: {
-      component: <FallbackAddress />,
-      title: (
-        <div className="flex items-center gap-1">
-          <Button
-            type="button"
-            variant="plain"
-            size="icon-xs"
-            onClick={() => wallet.setCandidate(undefined)}
-          >
-            <Left />
-          </Button>
-          Fallback Address
-        </div>
-      ),
-      dialogMinWidth: 'min-w-4xl'
     }
   };
 
@@ -68,7 +37,6 @@ export const WalletDialog = () => {
       onOpenChange={(isOpen) => !isOpen && wallet.closeDialog()}
       title={steps[state].title}
       description={steps[state].description}
-      dialogMinWidth={steps[state].dialogMinWidth}
     >
       {steps[state].component}
     </ConnectWalletDialog>
